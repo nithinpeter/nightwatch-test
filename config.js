bdf146ca05756529c3666d42f2fd467f3e9a21b9
Nithin Peter <peter@jugglestreet.com>
2016-04-22 16:39:07 +1000

    Update config.

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 module.exports = {
     dev: {
-        baseUrl: 'http://frontendserver:3000/'
+        baseUrl: 'http://frontendserver:3000/',
+        userName: 'nithinpeter7@gmail.com',
+        password: 'malayalam'
     },
     prod: {
         baseUrl: 'http://jugglestreet.com/'
